4fa620eed66fb990868131e206cfaa6310183ae8
creeperroyalytb <34447042+creeperroyalytb@users.noreply.github.com>
2022-01-11 03:51:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,3 +47,16 @@ for (const file of commandFiles) {
 	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 } 
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+} 
