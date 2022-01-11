3a9419ca23c9f6d7d1d77bd7918812346b8789a7
creeperroyalytb <34447042+creeperroyalytb@users.noreply.github.com>
2022-01-11 03:47:32 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,3 +22,18 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 } 
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
