8b2ea7b210449593cb3feb31407ca8a9e63242ee
creeperroyalytb <34447042+creeperroyalytb@users.noreply.github.com>
2022-01-11 04:23:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -72,4 +72,17 @@ for (const file of eventFiles) {
 	}
 } 
 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
    
